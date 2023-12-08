02866ff89cf8426f69e924eaf7378495cec26fea
raqib03 <72424440+raqib03@users.noreply.github.com>
2023-12-08 20:33:47 +0600

    Applied split() method to call user only by first name

--- Functions/hello.py
+++ Functions/hello.py
@@ -1,5 +1,9 @@
 # Ask user for their name
 name = input("What's your name? ").strip().title()
 
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
 # Say hello to user
-print(f"Hello, {name}!")
+print(f"Hello, {first}!")
+## this can split only two parts by space, first name and last name
